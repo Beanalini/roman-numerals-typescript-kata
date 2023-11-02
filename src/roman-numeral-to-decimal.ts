da4a3dba8d23e6dc58d0e7ae0a76ba56dbf891fc
Beanalini <dat826@gmail.com>
2023-11-02 22:42:07 +0000

    feat: add code to return decimal conversion o f higher numerals followed by lower

--- src/roman-numeral-to-decimal.ts
+++ src/roman-numeral-to-decimal.ts
@@ -2,7 +2,10 @@
 1] All Roman numerals have a direct mapping to a decimal number 
 [2] Convert numerals to decimal equivalent and sum the values
 [3] If  a letter of higher value follows a letter of lower value then the lower value is subtracted from the total value
+[4] If the following letter is the same value or higher then prev value is added
 */
+
+//use key value pairs for mapping this time
 
 type RomanMap = {
   [key: string]: number;
@@ -20,7 +23,13 @@ const romanMap: RomanMap = {
 export function romanNumeralToDecimal(romanInput: string): number {
   let count = 0;
   for (let i = 0; i < romanInput.length; i++) {
-    count += romanMap[romanInput[i]];
+    if (romanMap[romanInput[i]] >= romanMap[romanInput[i + 1]]) {
+      count += romanMap[romanInput[i]];
+      continue;
+    }
+
+    //last element reached
+    if (i + 1 === romanInput.length) count += romanMap[romanInput[i]];
   }
   return count;
 }
--- tests/romanNumeralToDecimal.test.ts
+++ tests/romanNumeralToDecimal.test.ts
@@ -7,10 +7,17 @@ describe("romanToNumeral()", () => {
     expect(romanNumeralToDecimal("C")).toEqual(100);
     expect(romanNumeralToDecimal("D")).toEqual(500);
   });
-  test("Test direct mapping of repeated numerals", () => {
+  test("Test direct mapping of repeated numerals e.g. II, XX", () => {
     expect(romanNumeralToDecimal("II")).toEqual(2);
     expect(romanNumeralToDecimal("III")).toEqual(3);
     expect(romanNumeralToDecimal("XX")).toEqual(20);
     expect(romanNumeralToDecimal("MMM")).toEqual(3000);
+  });
+  test("Tests higher numeral followed by lower", () => {
+    expect(romanNumeralToDecimal("XV")).toEqual(15);
+    expect(romanNumeralToDecimal("CL")).toEqual(150);
+    expect(romanNumeralToDecimal("CX")).toEqual(110);
+    expect(romanNumeralToDecimal("DC")).toEqual(600);
+    expect(romanNumeralToDecimal("MD")).toEqual(1500);
   });
 });
