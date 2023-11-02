99faee6ab30d0f7f261604398d23170a1bc201bd
Beanalini <dat826@gmail.com>
2023-11-02 23:01:39 +0000

    refactor: bracket notation and replace with variable in romanNumeralToDecimal function

--- src/roman-numeral-to-decimal.ts
+++ src/roman-numeral-to-decimal.ts
@@ -1,8 +1,9 @@
 /* Rules
 1] All Roman numerals have a direct mapping to a decimal number 
 [2] Convert numerals to decimal equivalent and sum the values
-[3] If  a letter of higher value follows a letter of lower value then the lower value is subtracted from the total value
+[3] If  a letter of higher value follows a letter of lower value then the lower value is subtracted 
 [4] If the following letter is the same value or higher then prev value is added
+
 */
 
 //use key value pairs for mapping this time
@@ -22,19 +23,23 @@ const romanMap: RomanMap = {
 
 export function romanNumeralToDecimal(romanInput: string): number {
   let count = 0;
+
   for (let i = 0; i < romanInput.length; i++) {
-    if (romanMap[romanInput[i]] >= romanMap[romanInput[i + 1]]) {
-      count += romanMap[romanInput[i]];
+    let currentMap = romanMap[romanInput[i]];
+    let nextMap = romanMap[romanInput[i + 1]];
+
+    if (currentMap >= nextMap) {
+      count += currentMap;
       continue;
     }
 
-    if (romanMap[romanInput[i]] < romanMap[romanInput[i + 1]]) {
-      count -= romanMap[romanInput[i]];
+    if (currentMap < nextMap) {
+      count -= currentMap;
       continue;
     }
 
     //last element reached
-    if (i + 1 === romanInput.length) count += romanMap[romanInput[i]];
+    if (i + 1 === romanInput.length) count += currentMap;
   }
   return count;
 }
